c3bfc31aa2ac05ae93255ab423d00b8b8a7ed81f
Julio Castillo <julio.castillo@continental-corporation.com>
2020-04-22 23:26:54 -0500

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/jobs")
 def jobs():
     return render_template("index.html")
-
-
-# @app.route("/jobs")
-# def jobs():
-#     return render_template("index.html")
